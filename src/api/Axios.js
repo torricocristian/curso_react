b318b59589662a80218005d59f9739442432ee3c
Cristian Torrico <cristian.torrico@contactica-i.com>
2022-07-15 11:39:05 -0300

    chore(credentials): Change password

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: "https://cors-anywhere.herokuapp.com/https://tiendita.wacpr.net/wp-json/app/v1",
   auth: {
     username: 'dev',
-    password: 'c0nt4ct1c4_2022'
+    password: 'TienditA123'
   }
 });
 
