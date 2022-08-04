d4046b165207ae5c7e5ecf82779d52c890883f19
Cristian Torrico <cristian.torrico@contactica-i.com>
2022-08-04 02:01:55 -0300

    feat(firebase): Añadir funcionalidad para agregar una orden

--- src/layouts/cart/ShoppingCart.jsx
+++ src/layouts/cart/ShoppingCart.jsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
 import { useCart } from '../../hooks/useCart';
 import ItemsCount from '../../components/ItemsCount';
 import { useParams, Link } from 'react-router-dom';
 import '../../assets/styles/fonts/riode.scss';
 import '../../assets/styles/pages/PageCart.scss';
 import '../../assets/styles/alert.scss';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 
 const ShoppingCart = () => {
 
-  const { increase, decrease, removeProduct, cartItems } = useCart();
+  const { increase, decrease, removeProduct, cartItems, total} = useCart();
   const { network } = useParams();
+  const [success, setSuccess] = useState(false);
 
-  console.log(cartItems)
+  const order = {
+    buyer:{
+      name: 'Cristian',
+      email: 'cristian@info.com',
+      phone: '223222',
+      address: 'Calle falsa 123'
+    },
+    items: cartItems.map(item => ({title: item.title, price: item.price, quantity: item.quantity})),
+    total: total
+  }
+
+  const handleClick = () => {
+    const db = getFirestore();
+    const orderCollection = collection(db, 'orders');
+    addDoc(orderCollection, order)
+    .then(({ id }) => id && setSuccess(true))
+  }
 
   function handlerQuantity(newQuantity, product) {
     if( newQuantity > product.quantity && newQuantity <= product.stock ){
@@ -25,10 +44,16 @@ const ShoppingCart = () => {
     removeProduct(product);
   }
 
+
   return (
     <main id="ShoppingCart">
       <section className="shop-table">
         <div className="container">
+
+          {
+            cartItems.length === 0 && <div className="alert alert__warning">No hay productos en el carrito. <Link to={`/${network}/productos`}>Añadir productos</Link></div>
+          }
+
           {
             cartItems.length > 0 &&
             <div className="row header">
@@ -71,15 +96,23 @@ const ShoppingCart = () => {
                 </div>
               </div>
             ) 
-          }
-
-
-          {
-            cartItems.length === 0 && <div className="alert alert__warning">No hay productos en el carrito. <Link to={`/${network}/productos`}>Añadir productos</Link></div>
-          }
+          } 
     
         </div>
       </section> 
+
+
+      <section className='cta'>
+          <div className="container">
+            <button className="btn btn-primary" onClick={()=>handleClick()}>
+              Enviar productos
+            </button>
+
+            {   
+              success && <div className="alert alert__warning">¡Pedido realizado!</div>
+            }
+          </div>
+      </section>
     </main>
   )
 }
